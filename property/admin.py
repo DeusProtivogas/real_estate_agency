8d1a288a9dbfac3067474a3af85aed490860f037
kiril <kirill2689@gmail.com>
2024-01-09 21:19:25 +0300

    added inlines

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,9 @@ from property.models import Flat
 
 from .models import Flat, Complaint, Owner
 
+class OwnersInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ["owner"]
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ["town", "owner", "address"]
@@ -11,6 +14,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ("new_building", )
     list_filter = ["new_building", "price"]
     raw_id_fields = ["liked_by", ]
+    inlines = [
+        OwnersInline,
+    ]
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ("flat", )
