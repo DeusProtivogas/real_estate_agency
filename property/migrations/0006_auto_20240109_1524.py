81ed300bc844b3d0bfdd5786a3e1b13cde31c050
kiril <kirill2689@gmail.com>
2024-01-10 17:40:48 +0300

    Added update to data migrations

--- property/migrations/0006_auto_20240109_1524.py
+++ property/migrations/0006_auto_20240109_1524.py
@@ -3,16 +3,14 @@ from django.db import migrations
 
 
 def set_new_building_status(apps, schema_editor):
-    flats = apps.get_model('property', 'Flat')
-    for flat in flats.objects.all():
-        flat.new_building = True if flat.construction_year >= 2015 else False
-        flat.save()
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.filter(construction_year__lt=2015).update(False)
+    Flat.objects.filter(construction_year__gte=2015).update(True)
+
 
 def move_backward(apps, schema_editor):
-    flats = apps.get_model('property', 'Flat')
-    for flat in flats.objects.all():
-        flat.new_building = None
-        flat.save()
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.update(None)
 
 
 class Migration(migrations.Migration):
